a8def043dd74c0ca7e36bd198562b40d2123dfc6
Tobias Kim <madeffort0903@gmail.com>
2024-08-14 10:18:31 +0900

    update

--- api/reservations/models.py
+++ api/reservations/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 from api.common.models import CommonModel
+from api.packages.models import Package
+from api.accounts.models import User
 
 
 class Reservation(CommonModel):
@@ -11,23 +13,36 @@ class Reservation(CommonModel):
         OPERATING = ("운행중", "Operating")
         DONE = ("서비스 완료", "Done")
         COMPLETE = ("결제 완료", "Complete")
+
     id = ShortUUIDField(max_length=128, primary_key=True, editable=False)
-    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name="reservations")
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations")
-    status = models.CharField(max_length=15, default=ReservationStatus.PENDING, choices=ReservationStatus.choices)
+    package = models.ForeignKey(
+        Package, on_delete=models.CASCADE, related_name="reservations"
+    )
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reservations"
+    )
+    status = models.CharField(
+        max_length=15,
+        default=ReservationStatus.PENDING,
+        choices=ReservationStatus.choices,
+    )
     package_name = models.CharField(max_length=150, default="")
     package_price = models.PositiveIntegerField(default=0)
     reservation_date = models.DateField(null=True, blank=True)
 
+
 class ReservationOption(CommonModel):
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="options")
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="options"
+    )
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
 
+
 class ReservationTimeSlot(CommonModel):
     id = ShortUUIDField(max_length=128, primary_key=True, editable=False)
-    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
     # 예약 가능한 날짜
     date = models.DateField()
     # 예약 가능 시작시간
@@ -36,4 +51,3 @@ class ReservationTimeSlot(CommonModel):
     end_time = models.TimeField()
     # 예약 가능 여부
     is_available = models.BooleanField(default=True)
-    
