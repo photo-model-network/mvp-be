0010e25ccbf375f9a2fbc7740551c72a4e9fa055
Tobias Kim <madeffort0903@gmail.com>
2024-08-21 13:37:34 +0900

    bank accounts verification done .

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from .views import GoogleView, NaverView, KakaoView
-from .views import BankVerificationView
+from .views import SendBankVerificationView, ConfirmBankVerificationView
 
 urlpatterns = (
     [
@@ -21,9 +21,17 @@ urlpatterns = (
         path("accounts/kakao/login/", KakaoView.as_view(), name="kakao_login"),
     ]
     + [
-        # 1원 계좌인증 (본인 계좌 등록)
+        # 1원 전송 및 인증 코드 저장
         path(
-            "accounts/bank/verify/", BankVerificationView.as_view(), name="bank_verify"
+            "accounts/bank/verification/send/",
+            SendBankVerificationView.as_view(),
+            name="bank_verification_send",
+        ),
+        # 인증 코드 일치 여부 확인
+        path(
+            "accounts/bank/verification/confirm/",
+            ConfirmBankVerificationView.as_view(),
+            name="bank_verification_confirm",
         ),
     ]
 )
