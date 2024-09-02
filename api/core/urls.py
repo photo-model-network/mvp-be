2f3de2aae89899ee2b867cb84dfb7112127f446e
InnovationPARK <innovation0220@gmail.com>
2024-09-02 20:38:34 +0900

    favorite artists

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from api.core.views import CoreSearchView
+# from api.core.views import CoreSearchView
 
 urlpatterns = [
-    path('core/search/', CoreSearchView.as_view(), name='core_search'),
+    # path('core/search/', CoreSearchView.as_view(), name='core_search'),
 ]
