fefbd6f750807dea89714a8516327287c5ed5802
Tobias Kim <madeffort0903@gmail.com>
2024-09-02 14:13:58 +0900

    preference table

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from api.accounts.models import User
+from api.common.models import CommonModel
 
-# Create your models here.
+
+class Preference(CommonModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 유저별로 선호도를 알 수 있는 것들 저장
