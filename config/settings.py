8648fa784185b5255d7991c598e0f80dea2e3c4b
Tobias Kim <madeffort0903@gmail.com>
2024-09-02 12:03:32 +0900

    docker config

--- config/settings.py
+++ config/settings.py
@@ -33,10 +33,10 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://mvp-be-production-8fc5.up.railway.app",
+    "https://*",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://mvp-be-production-8fc5.up.railway.app",
+    "https://*",
 ]
 
 X_FRAME_OPTIONS = "DENY"
@@ -112,23 +112,23 @@ ASGI_APPLICATION = "config.asgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("POSTGRES_DB", cast=str),
-#         "USER": config("POSTGRES_USER", cast=str),
-#         "PASSWORD": config("POSTGRES_PASSWORD", cast=str),
-#         "HOST": "postgres",
-#         "PORT": config("POSTGRES_PORT", cast=int),
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB", cast=str),
+        "USER": config("POSTGRES_USER", cast=str),
+        "PASSWORD": config("POSTGRES_PASSWORD", cast=str),
+        "HOST": "postgres",
+        "PORT": config("POSTGRES_PORT", cast=int),
+    }
+}
 
 CHANNEL_LAYERS = {
     "default": {
