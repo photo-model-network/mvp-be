b08ece4b724a7f70715c8b016f8a2d80701d97c1
Tobias Kim <madeffort0903@gmail.com>
2024-09-03 23:11:50 +0900

    for test

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-ENV = config("ENV", default="development")
+ENV = config("ENV", default="development", cast=str)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET", cast=str)
@@ -29,7 +29,7 @@ SECRET_KEY = config("DJANGO_SECRET", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-site_domain = config("RAILWAY_PUBLIC_DOMAIN", default="")
+site_domain = config("RAILWAY_PUBLIC_DOMAIN", default="", cast=str)
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
