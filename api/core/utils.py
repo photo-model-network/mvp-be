bcc45baf1c848f7b45c6b869d92ef2e49093454e
Gothax <scott990328@gmail.com>
2024-09-19 13:56:01 +0900

    주석 추가

--- api/core/utils.py
+++ api/core/utils.py
@@ -3,6 +3,7 @@ import os, uuid
 from django.core.files.storage import default_storage
 
 def save_images(images, package):
+    """이미지 여러장 저장"""
     image_objects = []
     for image in images:
         new_filename = save_image(image, package)
@@ -11,11 +12,13 @@ def save_images(images, package):
 
 
 def save_thumbnail(thumbnail, package):
+    """썸네일 저장"""
     new_filename = save_image(thumbnail, package)
     package.thumbnail = new_filename
     package.save()
     
 def save_image(image, package):
+    """이미지 한장 저장"""
     ext = os.path.splitext(image.name)[1]
     new_filename = os.path.join(
         f"packages/{package.id}/images", f"{uuid.uuid4()}{ext}"
