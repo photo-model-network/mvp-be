652d875d02b0f42196615e55a06efc0c11a89ffe
Gothax <scott990328@gmail.com>
2024-09-22 16:26:08 +0900

    thumbnail rollback

--- api/core/utils.py
+++ api/core/utils.py
@@ -10,18 +10,21 @@ def save_images(images, package):
         image_objects.append(PackagePicture(package=package, image=new_filename))
     PackagePicture.objects.bulk_create(image_objects)
 
-
-def save_thumbnail(thumbnail, package):
-    """썸네일 저장"""
-    new_filename = save_image(thumbnail, package)
-    package.thumbnail = new_filename
-    package.save()
-    
 def save_image(image, package):
-    """이미지 한장 저장"""
+    """packages/{id}/images/uuid.ext 경로로 이미지 저장"""
     ext = os.path.splitext(image.name)[1]
     new_filename = os.path.join(
         f"packages/{package.id}/images", f"{uuid.uuid4()}{ext}"
     )
     default_storage.save(new_filename, image)
     return new_filename
+
+def save_thumbnail(thumbnail, package):
+    """packages/{id}/thumbnail/uuid.ext 경로로 썸네일 저장"""
+    ext = os.path.splitext(thumbnail.name)[1]
+    new_filename = os.path.join(
+        f"packages/{package.id}/thumbnail", f"{uuid.uuid4()}{ext}"
+    )
+    default_storage.save(new_filename, thumbnail)
+    package.thumbnail = new_filename
+    package.save()
